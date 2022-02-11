fc118040303df67c52bd10d1ef349abbbd2adf32
raSKTypeShit <voltraxbocaj@gmail.com>
2022-02-11 11:20:28 +0100

    la til dbConn

---
+++ callforduty/elementer/dbConn.php
@@ -0,0 +1,13 @@
+<?php
+    $host = "localhost";
+    $usr = "root";
+    $pass = "root";
+    $db = "callforduty";
+
+    $conn = mysqli_connect($host, $usr, $pass, $db);
+
+    if(!$conn)
+    {
+        die("connection failed: " . mysqli_connect_error());
+    }
+?>
