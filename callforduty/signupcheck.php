69947bd3a10aaa7dcc558bb93a97faa5f1e5b83c
isak4307 <99408512+isak4307@users.noreply.github.com>
2022-02-18 11:33:24 +0100

    Update signupcheck.php

--- callforduty/signupcheck.php
+++ callforduty/signupcheck.php
@@ -1,10 +1,4 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "callforduty";
-$conn = mysqli_connect($servername, $username, $password, $database);
-?>
+
 <!DOCTYPE html>
 <html lang="en">
      <head>
@@ -13,6 +7,7 @@ $conn = mysqli_connect($servername, $username, $password, $database);
      </head>
      <body>
          <?php
+          include "include/connect.php";
          //sjekker om det finnes data i inputene.
          if(isset($_POST["name"]) && isset($_POST["mail"])&& isset($_POST["password"])){ 
             $name=$_POST["name"];
