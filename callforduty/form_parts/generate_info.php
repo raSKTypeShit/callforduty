009d9a3dd8c1b0473b63c49c1876dcc91f3a749c
Gunmy <seb.munthe@gmail.com>
2022-02-23 12:33:43 +0100

    Fargekoder

--- callforduty/form_parts/generate_info.php
+++ callforduty/form_parts/generate_info.php
@@ -1,5 +1,7 @@
 <?php
 
+include "./include/color_functions.php";
+
 $formNR = intval($_GET["formNR"]);
 
 $sql = "SELECT title, descr, COLOR, area FROM annonser WHERE id=" . $formNR;
@@ -8,11 +10,25 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) == 1) {
     $row = mysqli_fetch_assoc($result);
 
-    echo "<h2>" . $row["title"] . "</h2>";
-    echo "<style>body > main > h2 {background-color: " . $row["COLOR"] . ";}</style>";
+    $rgb = convert_hex_to_rgb_list($row["COLOR"]);
+    var_dump($rgb);
+    $bright = brighten($rgb, 0.1);
 
+    echo "<h2>" . $row["title"] . "</h2>";
     echo "<section><h3>Beskrivelse av jobb</h3><p>" . $row["descr"] . "</p>
     <address><p>Lokasjon: " . $row["area"] . "</p></address></section>";
+
+
+    echo "<style>
+    body > main > h2 {
+        background-color: " . $row["COLOR"] . ";
+    } 
+    body > main > section > h3 {
+        background-color: rgb(" . $bright[0] . ", " . $bright[1] . ", " . $bright[2] .");
+    }
+    </style>";
+
+    var_dump($rgb);
 
 } else { echo "0 results"; }
 
