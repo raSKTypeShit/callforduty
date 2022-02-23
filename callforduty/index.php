f1fb86e274cbfbd55adb65760be843f25a09c1d3
raSKTypeShit <voltraxbocaj@gmail.com>
2022-02-23 12:18:01 +0100

    include connect.php i index

--- callforduty/index.php
+++ callforduty/index.php
@@ -1,15 +1,5 @@
 <?php
-    $host = "localhost";
-    $usr = "root";
-    $pass = "";
-    $db = "callforduty";
-    session_start();
-    $conn = mysqli_connect($host, $usr, $pass, $db);
-
-    if(!$conn)
-    {
-        die("connection failed: " . mysqli_connect_error());
-    }
+    include "include/connect.php";
 ?>
 
 <!DOCTYPE html>
