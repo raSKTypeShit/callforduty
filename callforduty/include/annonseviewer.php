d2030c72fbaad21f8bd355ad7ad6d4e5dd47b3ad
isak4307 <isak4307@gmail.com>
2022-02-23 12:43:40 +0100

    testing

--- callforduty/include/annonseviewer.php
+++ callforduty/include/annonseviewer.php
@@ -1,7 +1,7 @@
 <section id="annonseviewer">
     <?php
 
-    include "dbConn.php";
+    include "connect.php";
 
     $sql = "SELECT * FROM annonser";
     $result = mysqli_query($conn, $sql);
@@ -13,6 +13,6 @@
                 echo "<article><h1>" . $row["title"] . "</h1><p id=\"pArea\">" . $row["area"] . "</p><p id=\"pCompany\">" . $row["userID"] . "</p><p id=\"pDesc\">" . $row["descr"] . "</p></article>";
             }
         }
-
+    mysqli_close($conn);
     ?>
 </section>
