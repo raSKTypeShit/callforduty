35c4dcbdbeee74d0f7efc46c1aaa1e7c138cf2b5
raSKTypeShit <voltraxbocaj@gmail.com>
2022-02-23 13:01:55 +0100

    annonseviewer update

--- callforduty/include/annonseviewer.php
+++ callforduty/include/annonseviewer.php
@@ -3,14 +3,14 @@
 
     include "dbConn.php";
 
-    $sql = "SELECT * FROM annonser";
+    $sql = "SELECT * FROM annonser JOIN login ON annonser.userID = login.id";
     $result = mysqli_query($conn, $sql);
 
     if(mysqli_num_rows($result))
     {
             while($row = mysqli_fetch_assoc($result))
             {
-                echo "<article><h1>" . $row["title"] . "</h1><p id=\"pArea\">" . $row["area"] . "</p><p id=\"pCompany\">" . $row["userID"] . "</p><p id=\"pDesc\">" . $row["descr"] . "</p></article>";
+                echo "<a href=\"" . $row["id"] . "\"><article><h1>" . $row["title"] . "</h1><p id=\"pArea\">" . $row["area"] . "</p><p id=\"pCompany\">" . $row["user"] . "</p><p id=\"pDesc\">" . $row["descr"] . "</p></article></a>";
             }
         }
 
