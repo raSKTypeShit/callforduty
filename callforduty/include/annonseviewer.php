29036217c247422d4d36a196c2d87dff1b702651
isak4307 <99408512+isak4307@users.noreply.github.com>
2022-02-25 18:11:02 +0100

    Update annonseviewer.php

--- callforduty/include/annonseviewer.php
+++ callforduty/include/annonseviewer.php
@@ -25,7 +25,7 @@
     $statement = mysqli_stmt_init($conn);
 
     // Base sql query
-    $sql = "SELECT * FROM annonser JOIN login ON annonser.userID = login.id";
+    $sql = "SELECT annonser.id AS annonseids, userID, title, descr, COLOR, area FROM annonser JOIN login ON  annonser.userID= login.id";
 
     // Area is spesified
     if($_GET["area"])
@@ -102,7 +102,7 @@
     {
         while($row = mysqli_fetch_assoc($result))
         {
-            echo "<a href=\"" . $row["id"] . "\"><article><h1>" . $row["title"] . "</h1><p id=\"pArea\">" . $row["area"] . "</p><p id=\"pCompany\">" . $row["user"] . "</p><p id=\"pDesc\">" . $row["descr"] . "</p></article></a>";
+            echo "<a href=\callforduty/callforduty/apply.php?formNR=" . $row["annonseids"]."><article><h1>" . $row["title"] . "</h1><p id=\"pArea\">" . $row["area"] . "</p><p id=\"pCompany\">" . $row["user"] . "</p><p id=\"pDesc\">" . $row["descr"] . "</p></article></a>";
         }
     }
 
