4b04e24a943484758a735f3abb26ecc1e24e5eb0
Gunmy <seb.munthe@gmail.com>
2022-02-26 22:31:02 +0100

    Statistikkside

--- callforduty/data_viewer.php
+++ callforduty/data_viewer.php
@@ -18,8 +18,6 @@
         }
     }
 
-    echo $properInfo;
-
     if ($properInfo != 1) {
         header('Location: index.php'); 
         exit();
@@ -34,34 +32,45 @@
     <link rel="stylesheet" href="styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
     
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script>
-        $("#link").click(function(){
-        var holdtext = $("#Clipboard").innerText;
-        Copied = holdtext.createTextRange();
-        Copied.execCommand("Copy");
-        });
-    </script>
 </head>
 
 <body class="data_viewer">
 
     <main>
+        <h2>Settings</h2>
         <section class="buttons">
-            <div><button onclick="copyToClipboard()">Copy URL</button>
-</div>
+            <div><button onclick="navigator.clipboard.writeText('localhost/callforduty/callforduty/apply.php?formNR=<?php echo $formNR ?>');">Copy URL</button></div>
             <div>Upload images</div>
             <div>View form</div>
             <div>Public</div>
             <div>Delete</div>
+            <div>Enkeltresponser</div>
         </section>
 
+        <h2>Info</h2>
+
         <section>
-            <article>
-                <h3>Spørsmål?</h3>
-                <svg height="100" width="100"></svg>
-            </article>
+<?php
+
+$sql = "SELECT title, descr, COLOR, area FROM annonser WHERE id=" . $formNR;
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) == 1) {
+    $row = mysqli_fetch_assoc($result);
+
+    echo "
+    <p><strong>Tittel:</strong>" . $row["title"] . "</p>
+    <p><strong>Lokasjon:</strong> " . $row["area"] . "</p>
+    <p><strong>Fargekode:</strong> " . $row["COLOR"] . "</p>
+    <p><strong>Beskrivelse av jobb: </strong>" . $row["descr"] . "</p>";
+
+
+} else { echo "0 results"; }
+
+
+?>
         </section>
+        <h2>Statistikk</h2>
     </main>
 </body>
 </html>
