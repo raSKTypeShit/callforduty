6318ea5b2b87100bf9f9da7fe4f12e32cfa5b2aa
Gunmy <seb.munthe@gmail.com>
2022-02-28 00:35:41 +0100

    Paiskjema fungerende

--- callforduty/form_parts/question_types/4.php
+++ callforduty/form_parts/question_types/4.php
@@ -1,5 +1,5 @@
 <?php
 
-echo '<input type="number" name="q' . $row["id"] . '" min="0" max="100">';
+echo '<input type="number" name="q' . $row["id"] . '" min="0" max="100" pattern=" 0+\.[0-9]*[1-9][0-9]*$" name="itemConsumption" onkeypress="return event.charCode >= 48 && event.charCode <= 57">';
 
 ?>
