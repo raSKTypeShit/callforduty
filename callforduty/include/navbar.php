5abef703eccdf07dacfee796c2cec18a6ddc72df
Gunmy <seb.munthe@gmail.com>
2022-03-10 14:09:58 +0100

    Navbar

--- callforduty/include/navbar.php
+++ callforduty/include/navbar.php
@@ -1,6 +1,14 @@
 <?php
+
+$height = 50;
+
 echo '
-<nav>
-    <a href="?"><img src="images/logo.png" width="150px"></a>
-</nav>';
+<nav class="navbar">
+    <a href="index.php"><img src="images/logo.png" height="' . $height . 'px"></a>';
+
+    if (isset($_SESSION["user"])) {
+        echo '<a href="profile_page.php"><img src="images/profile_page.png" height="' . $height . 'px"></a>';
+    }  
+
+echo '</nav>';
 ?>
